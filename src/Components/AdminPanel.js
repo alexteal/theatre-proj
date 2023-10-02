e597a1cd24fbee14e180192366801692b596986b
alexteal <29980694+alexteal@users.noreply.github.com>
2023-10-01 20:04:06 -0400

    database as own function

--- src/Components/AdminPanel.js
+++ src/Components/AdminPanel.js
@@ -1,32 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Css/AdminPanel.css";
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
-
-const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-function writeMovieData(movieName, movieUrl) {
-    const db = getDatabase();
-    const reference = ref(db, "movies/" + movieName);
-
-    set(reference, {
-        movie: movieName,
-        url: movieUrl,
-    });
-}
 
 const AdminPanel = ({
     currentlyRunning,
