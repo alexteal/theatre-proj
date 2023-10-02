4022fc00d42db2fcfaf7429692866c0828f0af62
alexteal <29980694+alexteal@users.noreply.github.com>
2023-10-02 10:50:27 -0400

    final commits

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -1,45 +1,54 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import './Css/Checkout.css';
+import "./Css/Checkout.css";
 
 function Checkout() {
-  const location = useLocation();
-  const { selectedShowtime, selectedSeats, price } = location.state.bookingData;
+    const location = useLocation();
+    const { selectedShowtime, selectedSeats, price } =
+        location.state.bookingData;
 
-  const handlePurchase = (e) => {
-    e.preventDefault();
+    const handlePurchase = (e) => {
+        e.preventDefault();
 
-    // Add any necessary logic for handling the purchase here, if needed
+        // Add any necessary logic for handling the purchase here, if needed
 
-    // Redirect to the homepage using window.location.href
-    window.location.href = '/';
-  };
+        // Redirect to the homepage using window.location.href
+        window.location.href = "/";
+    };
 
-  return (
-    <>
-      <div className='mainbox'>
-        <div className='content'>
-          <h1>Checkout</h1>
+    return (
+        <>
+            <div className="mainbox">
+                <div className="content">
+                    <h1>Checkout</h1>
 
-          <h2>Your Selections:</h2>
-          <p>Show Time: {selectedShowtime}</p>
-          <p>Seats: {selectedSeats.join(', ')}</p>
-          <p>Price: ${price}</p>
+                    <h2>Your Selections:</h2>
+                    <p>Show Time: {selectedShowtime}</p>
+                    <p>Seats: {selectedSeats.join(", ")}</p>
+                    <p>Price: ${price}</p>
 
-          <h2>Provide Your Information:</h2>
-          <form onSubmit={handlePurchase} className='formstuff'>
-            <input type="text" placeholder="First Name" required />
-            <input type="text" placeholder="Last Name" required />
-            <input type="email" placeholder="Email" required />
-            <input type="tel" placeholder="Phone Number" required />
-            <input type="text" placeholder="Credit Card Information" required />
-            <button type="submit">Purchase</button>
-          </form>
-        </div>
-      </div>
-    </>
-  );
+                    <h2>Provide Your Information:</h2>
+                    <form onSubmit={handlePurchase} className="formstuff">
+                        <input type="text" placeholder="First Name" required />
+                        <input type="text" placeholder="Last Name" required />
+                        <input type="email" placeholder="Email" required />
+                        <input type="tel" placeholder="Phone Number" required />
+                        <input
+                            type="text"
+                            placeholder="Credit Card Information"
+                            required
+                        />
+                        <button type="submit">Purchase</button>
+                        <Link to="/">
+                            <button type="exit"> Exit</button>
+                        </Link>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Checkout;
