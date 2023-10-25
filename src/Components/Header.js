b8a3b4f1b86d0a9b118da83e98c3c6babeb324f6
Gwalbrecher <cwalbrecherc@gmail.com>
2023-10-25 16:46:59 -0400

    Forgot To Stage Header Changes. Woops.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,8 +7,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Login from './Login';
 
-function Header() {
+function Header(props) {
+  const handleLogout = () => {
+    props.setIsLoggedIn(false);
+  };
+
+
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
@@ -17,16 +23,20 @@ function Header() {
           <Nav className="mr-auto">
             <Nav.Link href="/movies">Movies</Nav.Link>
             <Nav.Link href="/shows">Shows</Nav.Link>
-            <Nav.Link href="/signup">Signup</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <NavDropdown title="Welcome" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/edit">Edit Profile</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Purchases</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Log out
-              </NavDropdown.Item>
-            </NavDropdown>
+            {!props.isLoggedIn && (
+              <>
+                <Nav.Link href="/signup">Signup</Nav.Link> 
+                <Nav.Link href="/login">Login</Nav.Link>
+              </>
+            )}
+            {props.isLoggedIn && (
+              <NavDropdown title="Welcome" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/edit">Edit Profile</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Purchases</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item onClick={handleLogout}>Log out</NavDropdown.Item>
+              </NavDropdown>
+            )}
           </Nav>
         </Container>
       </Navbar>
