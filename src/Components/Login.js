2e1790acbafb53b00d29f41ac4ffe2c9b46a46f1
Gwalbrecher <cwalbrecherc@gmail.com>
2023-10-25 16:18:35 -0400

    Hooked Login up with <Links> to /AdminPanel and /

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import './Css/Login.css';
 import {checkUserPassword, checkUserType} from "./Database";
 import {AdminPanel} from "./AdminPanel";
-import {UserPage} from "./UserPage";
+import {Homepage} from "./Homepage";
 
 function LoginPage({ onLogin }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [redirectUrl, setRedirectUrl] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setisAdmin] = useState(false);
 
   const handleLogin = () => {
     if(checkUserPassword(email, password) = true){
-      if(checkUserType(email) = true){
-        setRedirectUrl('/adminpanel');
-      } else {
-        setRedirectUrl('/homepage');
-      }
-    } else {
-      setRedirectUrl('/login'); 
+      setIsLoggedIn(true);
+      setisAdmin(checkUserType(email));
     }
   };
+
+  if(checkUserPassword(email, password)) {
+    if(checkUserType(email)){
+      return <Link to="/AdminPanel" />;
+    } else {
+      return <Link to="/" />;
+    }
+  }
 
   return (
     <>
@@ -45,17 +49,9 @@ function LoginPage({ onLogin }) {
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
-              <Link to="/">
-                <button type="button" onClick={handleLogin}>
+              <button type="button" onClick={handleLogin}>
                   Login
-                </button>
-              </Link>
-
-              <Link to="/AdminPanel">
-                <button type="button" onClick={handleLogin}>
-                  Admin Panel
-                </button>
-              </Link>
+              </button>
             </form>
             <div className="links">
               <a href="#">Forget Password</a>
@@ -69,7 +65,6 @@ function LoginPage({ onLogin }) {
           <i style={{ '--clr': '#fffd44' }}></i>
         </div>
       </div>
-      {redirectUrl && <Redirect to={redirectUrl} />}
     </>
   );
 }
