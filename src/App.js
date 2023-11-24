2ac2866079bfc33f73dc3d9ac2afb56aecc2385b
alexteal <29980694+alexteal@users.noreply.github.com>
2023-11-24 17:46:07 -0500

    current working revision

--- src/App.js
+++ src/App.js
@@ -14,85 +14,75 @@ import OrderSummary from "./pages/order-summary/OrderSummary";
 import ManageMovies from "./pages/manage-movies/ManageMovies";
 
 function App() {
-  const { darkMode } = useContext(DarkModeContext);
-
-  const { currentUser } = useContext(AuthContext);
-  console.log("currentUser", currentUser);
-
-  const RequireAuth = ({ children }) => {
-    return currentUser ? children : <Navigate to="/login" />;
-  };
-  const RequireAdminAuth = ({ children }) => {
-    console.log(currentUser);
-    return currentUser && currentUser.isAdmin ? (
-      children
-    ) : (
-      <Navigate to="/login" />
+    const { darkMode } = useContext(DarkModeContext);
+    const { currentUser } = useContext(AuthContext);
+    console.log("currentUser", currentUser);
+    const RequireAuth = ({ children }) => {
+        return currentUser ? children : <Navigate to="/login" />;
+    };
+    const RequireAdminAuth = ({ children }) => {
+        console.log(currentUser);
+        return currentUser && currentUser.isAdmin ? (
+            children
+        ) : (
+            <Navigate to="/login" />
+        );
+    };
+    return (
+        <div className={darkMode ? "app dark" : "app"}>
+            <BrowserRouter>
+                <ProtectedRoutesChecker />
+                <Routes>
+                    <Route path="/">
+                        <Route path="login" element={<Login />} />
+                        <Route index element={<Home />} />{" "}
+                        {/* No authentication required for Home */}
+                        <Route path="/register" element={<Register />} />
+                        <Route
+                            path="/edit-profile"
+                            element={
+                                <RequireAuth>
+                                    <EditProfile />
+                                </RequireAuth>
+                            }
+                        />
+                        <Route
+                            path="/book/:movieTitle"
+                            element={
+                                <RequireAuth>
+                                    <TicketBooking />
+                                </RequireAuth>
+                            }
+                        />
+                        <Route
+                            path="/checkout"
+                            element={
+                                <RequireAuth>
+                                    <Checkout />
+                                </RequireAuth>
+                            }
+                        />
+                        <Route
+                            path="/order-details"
+                            element={
+                                <RequireAuth>
+                                    <OrderSummary />
+                                </RequireAuth>
+                            }
+                        />
+                        <Route
+                            path="/manage-movies"
+                            element={
+                                <RequireAdminAuth>
+                                    <ManageMovies />
+                                </RequireAdminAuth>
+                            }
+                        />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
     );
-  };
-
-  return (
-    <div className={darkMode ? "app dark" : "app"}>
-      <BrowserRouter>
-        <ProtectedRoutesChecker />
-        <Routes>
-          <Route path="/">
-            <Route path="login" element={<Login />} />
-            <Route
-              index
-              element={
-                <RequireAuth>
-                  <Home />
-                </RequireAuth>
-              }
-            />
-            <Route path="/register" element={<Register />} />
-            <Route
-              path="/edit-profile"
-              element={
-                <RequireAuth>
-                  <EditProfile />
-                </RequireAuth>
-              }
-            />
-
-            <Route
-              path="/book/:movieTitle"
-              element={
-                <RequireAuth>
-                  <TicketBooking />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/checkout"
-              element={
-                <RequireAuth>
-                  <Checkout />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/order-details"
-              element={
-                <RequireAuth>
-                  <OrderSummary />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="/manage-movies"
-              element={
-                <RequireAdminAuth>
-                  <ManageMovies />
-                </RequireAdminAuth>
-              }
-            />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
 }
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,45 +1,54 @@
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, sendEmailVerification, updatePassword, reauthenticateWithCredential, EmailAuthProvider } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    sendEmailVerification,
+    updatePassword,
+    reauthenticateWithCredential,
+    EmailAuthProvider,
+} from "firebase/auth";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { signOut, sendPasswordResetEmail } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId:process.env.REACT_APP_APP_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
 export const register = (email, password) => {
-  return createUserWithEmailAndPassword(auth, email, password);
+    return createUserWithEmailAndPassword(auth, email, password);
 };
 
 export const sendVerificationEmail = (user) => {
-  return sendEmailVerification(user);
+    return sendEmailVerification(user);
 };
 
 export const sendResetEmail = (email) => {
-  return sendPasswordResetEmail(auth, email);
+    return sendPasswordResetEmail(auth, email);
 };
 
 export const updateUserPassword = (newPassword) => {
-  const user = auth.currentUser;  // Get the current logged-in user
-  return updatePassword(user, newPassword);
+    const user = auth.currentUser; // Get the current logged-in user
+    return updatePassword(user, newPassword);
 };
 
 export const reauthenticateUser = (currentPassword) => {
-  const user = auth.currentUser;
-  const credential = EmailAuthProvider.credential(user.email, currentPassword);
-  return reauthenticateWithCredential(user, credential);
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(
+        user.email,
+        currentPassword
+    );
+    return reauthenticateWithCredential(user, credential);
 };
 
 export const signOutUser = () => {
-  return signOut(auth);
+    return signOut(auth);
 };
 
 const app = initializeApp(firebaseConfig);
