ca0b15ea4e4691e858e5f240d92234aebacdbe0f
alexteal <29980694+alexteal@users.noreply.github.com>
2023-11-25 15:03:44 -0500

    this doesn't work

--- src/components/email/SendEmail.js
+++ src/components/email/SendEmail.js
@@ -1,17 +1,21 @@
 import sgMail from "@sendgrid/mail";
+
+// For the record, this is really stupid and should only be done on the server's
+// end. However, I don't have a credit card on this account so who cares if
+// someone steals the api key ¯\_(ツ)_/¯
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 export const sendEmail = async (to, from, subject, text, html) => {
-    const msg = {
-        to: to,
-        from: from,
-        subject: subject,
-        text: text,
-        html: html,
-    };
-    try {
-        await sgMail.send(msg);
-        console.log("Email sent");
-    } catch (error) {
-        console.error(error);
-    }
+  const msg = {
+    to: to,
+    from: from,
+    subject: subject,
+    text: text,
+    html: html,
+  };
+  try {
+    await sgMail.send(msg);
+    console.log("Email sent");
+  } catch (error) {
+    console.error(error);
+  }
 };
