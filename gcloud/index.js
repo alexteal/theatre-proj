8e7d54e978eee787f1c44d9712acb039044cb9f0
alexteal <29980694+alexteal@users.noreply.github.com>
2023-11-26 18:34:46 -0500

    fix cors

--- gcloud/index.js
+++ gcloud/index.js
@@ -1,16 +1,17 @@
 const sgMail = require("@sendgrid/mail");
 
-const functions = require("@google-cloud/functions-framework");
-
-functions.http("sendEmail", (req, res) => {
+exports.sendEmail = (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
+
   if (req.method === "OPTIONS") {
     // Send response to OPTIONS requests
-    res.set("Access-Control-Allow-Methods", "GET");
-    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Allow-Methods", "GET, POST");
+    res.set("Access-Control-Allow-Headers", "*");
     res.set("Access-Control-Max-Age", "3600");
+    res.set("Access-Control-Allow-Origin", "*");
     res.status(204).send("");
   }
+
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const { to, from, subject, text, html } = req.body;
   const msg = {
@@ -30,4 +31,4 @@ functions.http("sendEmail", (req, res) => {
       console.error(error);
       res.status(500).send("Error sending email");
     });
-});
+};
