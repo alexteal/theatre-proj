b73d9561b34abf49c51fb3a0924449397a459fd7
alexteal <29980694+alexteal@users.noreply.github.com>
2023-11-28 12:12:57 -0500

    i needed a walk after doxing that

--- gcloud/index.js
+++ gcloud/index.js
@@ -1,37 +1,40 @@
-const functions = require("firebase-functions");
+//gcloud functions deploy sendEmail --gen2 --runtime nodejs14 --trigger-http --allow-unauthenticated 
+const functions = require("@google-cloud/functions-framework");
 const sgMail = require("@sendgrid/mail");
 
-exports.sendEmail = functions.https.onRequest((req, res) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.set("Access-Control-Allow-Methods", "GET, POST");
-  res.set("Access-Control-Allow-Headers", "*");
-  res.set("Access-Control-Max-Age", "3600");
-  if (req.method === "OPTIONS") {
-    // Send response to OPTIONS requests
-    res.set("Access-Control-Allow-Methods", "GET, POST");
-    res.set("Access-Control-Allow-Headers", "*");
-    res.set("Access-Control-Max-Age", "3600");
-    res.set("Access-Control-Allow-Origin", "*");
-    res.status(204).send("");
-  } else {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const { to, from, subject, text, html } = req.body;
-    const msg = {
-      to: to,
-      from: from,
-      subject: subject,
-      text: text,
-      html: html,
-    };
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log("Email sent");
-        res.status(200).send("Email sent");
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("Error sending email");
-      });
+// Set your SendGrid API key here
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// Define the Cloud Function
+functions.http("sendEmail", (req, res) => {
+  // Only allow POST requests
+  if (req.method !== "POST") {
+    res.status(405).send("Method Not Allowed");
+    return;
   }
-});
+
+  const { to, from, subject, text, html } = req.body;
+
+  // Create the email
+  const msg = {
+    to: to,
+    from: from,
+    subject: subject,
+    text: text,
+    html: html,
+  };
+
+  // Send the email
+  sgMail
+    .send(msg)
+    .then(() => {
+      res.status(200).send("Email sent successfully");
+    })
+    .catch((error) => {
+      console.error(error);
+      if (error.response) {
+        console.error(error.response.body);
+      }
+      res.status(500).send("Error sending email");
+    });
+}
